b7cd8ef3b4b63481a1c98b820bcf4011331cf8de
Robert Fischer <artiefischer@gmail.com>
2018-11-11 16:55:51 -0700

    ☄︎

---
+++ accumulators.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Accumulators!", math.Sqrt(16))
+}
+
+//If you pass it "abcdef" return "ace"
+func everyOther(string) {
+
+}
+
+//If you pass 1 it should return [1]
+//If you pass 3 it should return [1,2,3]
+
+func arrayCounter(x int) {
+
+}
