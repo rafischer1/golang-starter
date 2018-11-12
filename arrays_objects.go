2215e7d6c4d4e0006d2fc6cfde253d25fd7b55b0
Robert Fischer <artiefischer@gmail.com>
2018-11-12 10:50:23 -0700

    ⚕︎

--- arrays_objects.go
+++ arrays_objects.go
@@ -5,5 +5,24 @@ import (
 )
 
 func main() {
-	fmt.Printlin("Arrays!")
+	fmt.Println("Arrays!")
+	var a [3]int
+	fmt.Println("emp:", a)
+	fmt.Println("length a:", len(a))
+
+	var b [5]int
+	b[4] = 100
+	fmt.Println("set:", b[4])
+
+	c := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("set c:", c)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+
 }
