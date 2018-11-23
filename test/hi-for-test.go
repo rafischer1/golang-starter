d9e1ec070ff3d0c52dd5bb8c2623c796a9de5bcb
Robert Fischer <artiefischer@gmail.com>
2018-11-23 10:25:46 -0700

    testing suite added

---
+++ test/hi-for-test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sum(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println("Hello World", Sum(5, 5))
+	Sum(3, 3)
+}
---
+++ test/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	tables := []struct {
+		x int
+		y int
+		n int
+	}{
+		{1, 1, 2},
+		{1, 2, 3},
+		{2, 2, 4},
+		{5, 2, 7},
+	}
+
+	for _, table := range tables {
+		total := Sum(table.x, table.y)
+		fmt.Printf("Sum of (%d+%d) was correct, got: %d, want: %d.", table.x, table.y, total, table.n)
+		if total != table.n {
+			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.n)
+		}
+	}
+}
